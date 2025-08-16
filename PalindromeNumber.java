83bc266ec5698e137c2d6cfa04c9b68c7d30dff8
SparksAndCircuits <warrenandrade@live.com>
2025-08-16 16:58:55 -0400

    Adding different Versions of isPalindrome.

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -33,7 +33,36 @@ public class PalindromeNumber {
         return tail;
     }
 
-    public boolean isPalindrome(int x) {
+    private void buildList(int x) {
+        head = null;
+        tail = null;
+        length = 0;
+
+        if (x == 0) {
+            Node node = new Node(0);
+            head = tail = node;
+            length = 1;
+            return;
+        }
+
+        while (x > 0) {
+            int digit = x % 10;
+            Node node = new Node(digit);
+
+            if (head == null) {
+                head = tail = node;
+            } else {
+                node.next = head;
+                head.prev = node;
+                head = node;
+            }
+
+            length++;
+            x /= 10;
+        }
+    }
+
+    public boolean isPalindrome1(int x) {
         if (length <= 1) {
             return true;
         }
@@ -48,6 +77,28 @@ public class PalindromeNumber {
 
             frontNode = frontNode.next;
             backNode = backNode.next;
+        }
+
+        return true;
+    }
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        buildList(x);
+
+        Node frontNode = head;
+        Node backNode = tail;
+
+        for (int i = 0; i < length / 2; i++) {
+            if (frontNode.value != backNode.value) {
+                return false;
+            }
+
+            frontNode = frontNode.next;
+            backNode = backNode.prev;
         }
 
         return true;
