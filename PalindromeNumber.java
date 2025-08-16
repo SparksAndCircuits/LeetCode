52e8a1b0ff223579b152d7160fb05ee1d6746e63
SparksAndCircuits <warrenandrade@live.com>
2025-08-16 14:10:25 -0400

    Palindrome Number:
    Given an integer x, return true if x is a palindrome, and false otherwise.

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,3 +1,55 @@
+/*
+ * 9. Palindrome Number
+ * Given an integer x, return true if x is a palindrome, and false otherwise.
+ */
+
 public class PalindromeNumber {
-    
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private PalindromeNumber(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public boolean isPalindrome(int x) {
+        if (length <= 1) {
+            return true;
+        }
+
+        Node frontNode = head;
+        Node backNode = head;
+
+        for (int i = 0; i < length / 2; i++) {
+            if (frontNode.value != backNode.value) {
+                return false;
+            }
+
+            frontNode = frontNode.next;
+            backNode = backNode.next;
+        }
+
+        return true;
+    }
 }
