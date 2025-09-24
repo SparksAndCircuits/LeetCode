72e33f9b4dc22e0e571a7871d674b6fa21cd9514
SparksAndCircuits <warrenandrade@live.com>
2025-09-23 21:53:05 -0400

    LeetCode question 66: plus one.
    Correct solution.

--- src/main/java/leetcode/easy/PlusOne.java
+++ src/main/java/leetcode/easy/PlusOne.java
@@ -8,20 +8,17 @@ package leetcode.easy;
  */
 public class PlusOne {
     public int[] plusOne(int[] digits){
-        int[] results = null;
-        for(int i = digits.length; i > 0; i--){
-            int n = i - 1;
-            int j;
-           
-            if(digits[i] <= 8){
-                j = digits[i + 1];
-                 results += digits[i];
-            }else{
-                digits[i] = 0;
-                n = digits[i + 1];
+        for(int i = digits.length - 1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
             }
 
-            return results;
+            digits[i] = 0;
         }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
     }
 }
