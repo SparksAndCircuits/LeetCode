eb602fd280b97e55c7645c47fd688af27ab33247
SparksAndCircuits <warrenandrade@live.com>
2025-10-04 18:39:11 -0400

    Leetcode question 108: Convert sorted array to binary search tree

--- src/main/java/leetcode/easy/SortedArrayToBST.java
+++ src/main/java/leetcode/easy/SortedArrayToBST.java
@@ -12,19 +12,24 @@ package leetcode.easy;
 
 public class SortedArrayToBST {
     public TreeNode sortedArrayToBST(int[] nums){
-        TreeNode root = new TreeNode();
-
-        for(int i = 0; i < nums.length; i++){
-            if(root == null){
-                nums[i] = root.val;
-            }else if(root.left == null){
-                nums[i] = root.left.val;
-            }else if(root.right == null){
-                nums[i] = root.right.val;
-            }else{
-                sortedArrayToBST(nums);
-            }
+        if(nums == null || nums.length == 0){
+            return null;
         }
+
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildBST(int[] nums, int left, int right){
+        if(left > right){
+            return null;
+        }
+
+        int mid = left + (right - left)/2;
+        TreeNode root = new TreeNode(nums[mid]);
+
+        root.left = buildBST(nums, left, mid - 1);
+        root.right = buildBST(nums, mid + 1, right);
+
         return root;
     }
 }
