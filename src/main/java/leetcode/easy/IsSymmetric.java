178325d36900a2f38541a2867cbbfb4fc7fbaad9
SparksAndCircuits <warrenandrade@live.com>
2025-10-04 11:29:49 -0400

    LeetCode question 101: Symmetric tree

--- src/main/java/leetcode/easy/IsSymmetric.java
+++ src/main/java/leetcode/easy/IsSymmetric.java
@@ -15,20 +15,18 @@ public class IsSymmetric {
         if (root == null) {
             return true;
         }
+        return isMirror(root.left, root.right);
+    }
 
-        if (root.left == root.right) {
-            if (root.left.left == root.right.right) {
-                if (root.left.right == root.right.left) {
-                    return true;
-                }else{
-                    return false;
-                }
-            }else{
-                return false;
-            }
-        }else{
+    public boolean isMirror(TreeNode left, TreeNode right){
+        if(left == null && right == null){
+            return true;
+        }
+
+        if(left == null || right == null){
             return false;
         }
 
+        return (left.val == right.val) && isMirror(left.left, right.right) && isMirror(left.right, right.left);
     }
 }
